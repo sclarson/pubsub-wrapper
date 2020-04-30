4e715e1930872491b9713a3372e9bfb63a2d3846
Seth Larson <seth.larson@gmail.com>
2020-04-30 14:04:40 -0500

    Add eslint and fix errors

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,41 +1,42 @@
+/* eslint-env node, mocha */
+
 import * as chai from 'chai'
-import { expect, should } from 'chai'
-chai.use(require('chai-events'))
+import { expect } from 'chai'
 import { PubSub, RedisConfig, StatsDConfig } from './index'
-import * as redis from 'redis'
+chai.use(require('chai-events'))
 
 describe('PubSub', () => {
-    describe('constructor', () => {
-        it('uses default localhost and port for config', () => {
-            try {
-                const pubsub = new PubSub()
-                expect(pubsub.config.statsD.host).to.equal('localhost')
-                expect(pubsub.config.statsD.port).to.equal(8125)
-                expect(pubsub.config.redis.host).to.equal('localhost')
-                expect(pubsub.config.redis.port).to.equal(6379)
-            } catch(err) {
-                console.log(err)
-            }
-        })
+  describe('constructor', () => {
+    it('uses default localhost and port for config', () => {
+      try {
+        const pubsub = new PubSub()
+        expect(pubsub.config.statsD.host).to.equal('localhost')
+        expect(pubsub.config.statsD.port).to.equal(8125)
+        expect(pubsub.config.redis.host).to.equal('localhost')
+        expect(pubsub.config.redis.port).to.equal(6379)
+      } catch (err) {
+        console.log(err)
+      }
     })
+  })
 
-    describe('publish and subscribe', () => {
-        it('can publish and listen on a channel', (done) => {
-            const channel = 'resources'
-            const pubsub = new PubSub(
-                new StatsDConfig('localhost', 6379),
-                new RedisConfig('localhost', 6379)
-            )
+  describe('publish and subscribe', () => {
+    it('can publish and listen on a channel', (done) => {
+      const channel = 'resources'
+      const pubsub = new PubSub(
+        new StatsDConfig('localhost', 6379),
+        new RedisConfig('localhost', 6379)
+      )
 
-            pubsub.subscribe(channel)
+      pubsub.subscribe(channel)
 
-            pubsub.on(channel, (message) => {
-                expect(message).to.deep.equal({"expecto":"patronum"})
-                done()
-            })
-            setTimeout(()=>{
-                pubsub.publish(channel, { "expecto": "patronum" })
-            },10)
-        })
+      pubsub.on(channel, (message) => {
+        expect(message).to.deep.equal({ expecto: 'patronum' })
+        done()
+      })
+      setTimeout(() => {
+        pubsub.publish(channel, { expecto: 'patronum' })
+      }, 10)
     })
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
-import { Stats } from "fs"
 import * as redis from 'redis'
 import { EventEmitter } from 'events'
-import { throws } from "assert"
+import * as StatsDClient from 'statsd-client'
 
 export class StatsDConfig {
     host: string
     port: number
 
-    constructor(host: string = 'localhost', port: number = 8125) {
-        this.host = host,
-        this.port = port
+    constructor (host: string = 'localhost', port: number = 8125) {
+      this.host = host
+      this.port = port
     }
 }
 
 export class RedisConfig {
     host: string
     port: number
-    constructor(host: string = 'localhost', port: number = 6379){
-        this.host = host
-        this.port = port
+    constructor (host: string = 'localhost', port: number = 6379) {
+      this.host = host
+      this.port = port
     }
 }
 
-export class PubSubConfig{
-    constructor(statsDConfig: StatsDConfig = new StatsDConfig(), redisConfig: RedisConfig = new RedisConfig()){
-        this.statsD = statsDConfig
-        this.redis = redisConfig
-    }
+export class PubSubConfig {
+  constructor (statsDConfig: StatsDConfig = new StatsDConfig(), redisConfig: RedisConfig = new RedisConfig()) {
+    this.statsD = statsDConfig
+    this.redis = redisConfig
+  }
+
     statsD: StatsDConfig
     redis: RedisConfig
 }
@@ -38,100 +38,99 @@ export class PubSub extends EventEmitter {
     _publisherReady: boolean
     _subscriberready: boolean
     logger: any
-    subscribe(channel: string) {
-        const inc = this._subscriptions.get(channel) + 1
-        if (isNaN(inc)) {
-            this._subscriptions.set(channel, 1)
-            this.logger.debug(`Subscribing to ${channel}`)
-            this._subscriber.subscribe(channel)
-        } else {
-            this._subscriptions.set(channel, inc)
-        }
+    _sdc: any
+    subscribe (channel: string) {
+      const inc = this._subscriptions.get(channel) + 1
+      if (isNaN(inc)) {
+        this._subscriptions.set(channel, 1)
+        this.logger.debug(`Subscribing to ${channel}`)
+        this._subscriber.subscribe(channel)
+      } else {
+        this._subscriptions.set(channel, inc)
+      }
     }
 
-    publish(channel: string, message: any) {
-        this.logger.debug(`Publishing to ${channel}`)
-        this._publisher.publish(channel, JSON.stringify(message))
-        this.emit('data', channel, JSON.stringify(message))
-    } 
+    publish (channel: string, message: any) {
+      this.logger.debug(`Publishing to ${channel}`)
+      this._publisher.publish(channel, JSON.stringify(message))
+      this.emit('data', channel, JSON.stringify(message))
+    }
 
     config: PubSubConfig
-    constructor(
-        statsDConfig = new StatsDConfig(),
-        redisConfig = new RedisConfig(),
-        logger = { debug: (param: string) => { console.log(param) }}
+    constructor (
+      statsDConfig = new StatsDConfig(),
+      redisConfig = new RedisConfig(),
+      logger = { debug: (param: string) => { console.log(param) } }
     ) {
-        super()
-        this.logger = logger
-        this._subscriptions = new Map()
-        this.config = new PubSubConfig(statsDConfig, redisConfig)
-        console.log(this.config.redis)
-        this._publisher = redis.createClient(this.config.redis.port, this.config.redis.host)
-        this._subscriber = redis.createClient(this.config.redis.port, this.config.redis.host)
-        
-        this._publisher.on('ready', () => {
-            this.logger.debug(`Publisher Ready`)
-            this._publisherReady = true
-        })
+      super()
+      this.logger = logger
+      this._subscriptions = new Map()
+      this.config = new PubSubConfig(statsDConfig, redisConfig)
+      console.log(this.config.redis)
+      this._sdc = new StatsDClient({ host: this.config.statsD.host, port: this.config.statsD.port })
+      this._publisher = redis.createClient(this.config.redis.port, this.config.redis.host)
+      this._subscriber = redis.createClient(this.config.redis.port, this.config.redis.host)
 
+      this._publisher.on('ready', () => {
+        this.logger.debug('Publisher Ready')
+        this._publisherReady = true
+      })
 
-        this._subscriber.on('ready', () => {
-            this.logger.debug(`Subscriber Ready`)
-            this._subscriberready = true
-        })
+      this._subscriber.on('ready', () => {
+        this.logger.debug('Subscriber Ready')
+        this._subscriberready = true
+      })
 
-        this._publisher.on('connect', () => {
-            this.logger.debug(`Publisher connecting ${this._publisher.connected}`)
+      this._publisher.on('connect', () => {
+        this.logger.debug(`Publisher connecting ${this._publisher.connected}`)
+      })
 
-        })
+      this._subscriber.on('connect', () => {
+        this.logger.debug(`Subscriber connecting ${this._subscriber.connected}`)
+        this.logger.debug(`Subscriber connecting ${(this._subscriber as any).status}`)
+      })
 
-        this._subscriber.on('connect', () => {
-            this.logger.debug(`Subscriber connecting ${this._subscriber.connected}`)
-            this.logger.debug(`Subscriber connecting ${(this._subscriber as any).status}`)
-        })
+      this._publisher.on('reconnecting', (delay, attempt) => {
+        this.logger.debug('Publisher reconnecting', delay, attempt)
+      })
 
-        this._publisher.on('reconnecting', (delay, attempt) => {
-            this.logger.debug('Publisher reconnecting', delay, attempt)
-        })
+      this._subscriber.on('reconnecting', (delay, attempt) => {
+        this.logger.debug('Subscriber reconnecting', delay, attempt)
+      })
 
-        this._subscriber.on('reconnecting', (delay, attempt) => {
-            this.logger.debug('Subscriber reconnecting', delay, attempt)
-        })
+      this._publisher.on('warning', (data) => {
+        this.logger.debug('Publisher connecting', data)
+      })
 
-        this._publisher.on('warning', (data) => {
-            this.logger.debug('Publisher connecting', data)
-        })
+      this._subscriber.on('warning', (data) => {
+        this.logger.debug('Subscriber warning', data)
+      })
 
-        this._subscriber.on('warning', (data) => {
-            this.logger.debug('Subscriber warning', data)
-        })
+      this._publisher.on('end', () => {
+        this.logger.debug('Publisher ended')
+      })
 
+      this._subscriber.on('end', () => {
+        this.logger.debug('Subscriber ended')
+      })
 
-        this._publisher.on('end', () => {
-            this.logger.debug('Publisher ended')
-        })
+      this._publisher.on('error', (err) => {
+        this.logger.debug(err)
+        this.emit('error', err)
+      })
 
-        this._subscriber.on('end', () => {
-            this.logger.debug('Subscriber ended')
-        })
+      this._subscriber.on('message', (schannel, smessage) => {
+        this.logger.debug(`Received message now emitting on channel: ${schannel}`)
+        this.emit(schannel, JSON.parse(smessage))
+      })
 
-        this._publisher.on('error', (err) => {
-            this.logger.debug(err)
-            this.emit('error', err)
-        })
-
-        this._subscriber.on('message', (schannel, smessage) => {
-            this.logger.debug(`Received message now emitting on channel: ${schannel}`)
-            this.emit(schannel, JSON.parse(smessage))
-        })
-
-        this._subscriber.on('error' ,(err) => {
-            this.logger.debug(err)
-            this.emit('error', err)
-        })
+      this._subscriber.on('error', (err) => {
+        this.logger.debug(err)
+        this.emit('error', err)
+      })
     }
 
-    ready() {
-        return this._publisherReady && this._subscriberready
+    ready () {
+      return this._publisherReady && this._subscriberready
     }
 }
