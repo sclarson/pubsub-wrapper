166b34c522e017a41daeda3ef8caea53c840f25e
Seth Larson <seth.larson@gmail.com>
2020-05-04 15:48:14 -0500

    Also send to statsd

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import * as StatsDClient from 'statsd-client'
 export class StatsDConfig {
     host: string
     port: number
+    instance: any
 
-    constructor (host: string = 'localhost', port: number = 8125) {
+    constructor (host: string = 'localhost', port: number = 8125, statsDInstance: any = null) {
       this.host = host
       this.port = port
+      this.instance = statsDInstance
     }
 }
 
@@ -39,24 +41,7 @@ export class PubSub extends EventEmitter {
     _subscriberready: boolean
     logger: any
     _sdc: any
-    subscribe (channel: string) {
-      const inc = this._subscriptions.get(channel) + 1
-      if (isNaN(inc)) {
-        this._subscriptions.set(channel, 1)
-        this.logger.debug(`Subscribing to ${channel}`)
-        this._subscriber.subscribe(channel)
-      } else {
-        this._subscriptions.set(channel, inc)
-      }
-    }
 
-    publish (channel: string, message: any) {
-      this.logger.debug(`Publishing to ${channel}`)
-      this._publisher.publish(channel, JSON.stringify(message))
-      this.emit('data', channel, JSON.stringify(message))
-    }
-
-    config: PubSubConfig
     constructor (
       statsDConfig = new StatsDConfig(),
       redisConfig = new RedisConfig(),
@@ -67,7 +52,13 @@ export class PubSub extends EventEmitter {
       this._subscriptions = new Map()
       this.config = new PubSubConfig(statsDConfig, redisConfig)
       console.log(this.config.redis)
-      this._sdc = new StatsDClient({ host: this.config.statsD.host, port: this.config.statsD.port })
+
+      if (this.config.statsD.instance !== null) {
+        this._sdc = this.config.statsD.instance
+      } else {
+        this._sdc = new StatsDClient({ host: this.config.statsD.host, port: this.config.statsD.port })
+      }
+
       this._publisher = redis.createClient(this.config.redis.port, this.config.redis.host)
       this._subscriber = redis.createClient(this.config.redis.port, this.config.redis.host)
 
@@ -116,20 +107,41 @@ export class PubSub extends EventEmitter {
 
       this._publisher.on('error', (err) => {
         this.logger.debug(err)
+        this._sdc.increment('pubusb.publisher.error')
         this.emit('error', err)
       })
 
       this._subscriber.on('message', (schannel, smessage) => {
         this.logger.debug(`Received message now emitting on channel: ${schannel}`)
+        this._sdc.increment(`pubsub.subscriber.${schannel}`)
         this.emit(schannel, JSON.parse(smessage))
       })
 
       this._subscriber.on('error', (err) => {
         this.logger.debug(err)
+        this._sdc.increment('pubusb.subscriber.error')
         this.emit('error', err)
       })
     }
 
+    subscribe (channel: string) {
+      const inc = this._subscriptions.get(channel) + 1
+      if (isNaN(inc)) {
+        this._subscriptions.set(channel, 1)
+        this.logger.debug(`Subscribing to ${channel}`)
+        this._subscriber.subscribe(channel)
+      } else {
+        this._subscriptions.set(channel, inc)
+      }
+    }
+
+    publish (channel: string, message: any) {
+      this.logger.debug(`Publishing to ${channel}`)
+      this._sdc.increment(`pubsub.publisher.${channel}`)
+      this._publisher.publish(channel, JSON.stringify(message))
+    }
+
+    config: PubSubConfig
     ready () {
       return this._publisherReady && this._subscriberready
     }
